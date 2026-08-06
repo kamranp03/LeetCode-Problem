b8c57576b0577338e005234e9fa2a8f1c24426ef
Kamran Pathan <kamranpathan0307@gmail.com>
2026-08-06 20:21:54 +0530

    Time: 45 ms (16.69%), Space: 47.6 MB (39.37%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> mp(26,0);
+
+        for(char ch: tasks)
+        {
+            mp[ch- 'A']++;
+        }
+
+        int time=0;
+        priority_queue<int> pq;//max heap
+
+        for(int i=0;i<26;i++)
+        {
+            if(mp[i]>0)
+            {
+                pq.push(mp[i]);
+            }
+        }
+
+        while(!pq.empty())
+        {
+            vector<int> temp;// to store freq after using 
+            for(int i=1;i<=n+1;i++)
+            {
+                if(!pq.empty())
+                {
+                    int freq= pq.top();
+                    pq.pop();
+                    freq--;
+                    temp.push_back(freq);
+                }
+            }
+
+            for(int f: temp)
+            {
+                if(f>0)
+                {
+                    pq.push(f);
+                }
+            }
+
+            if(pq.empty())  // last freq remaining
+            {
+                time+= temp.size();
+            }else{ // normal every iteration take n+1
+                time+= n+1;
+            }
+        }
+        return time;
+    }
+};
