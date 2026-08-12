563534a022bbae60b55c73b43a1816ee13eb8bee
Kamran Pathan <kamranpathan0307@gmail.com>
2026-08-12 19:14:53 +0530

    Time: 31 ms (53.26%), Space: 107.6 MB (52.92%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+
+        unordered_map<int, int> mp;
+
+        // Count frequency of each task
+        for (int task : tasks)
+            mp[task]++;
+
+        int round = 0;
+
+        for (auto it : mp)
+        {
+            int count = it.second;
+
+            // only 2 / 3 
+            if (count == 1)
+                return -1;
+
+            // Prefer groups of 3 (greedy)
+            if (count % 3 == 0)
+            {
+                round += count / 3;
+            }
+            else
+            {
+                // another grp of 2
+                round += count / 3 + 1;
+            }
+        }
+
+        return round;
+    }
+};
